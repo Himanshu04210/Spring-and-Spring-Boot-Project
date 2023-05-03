35727671923a8a2ad2b166bb146f637ebb70b5d7
Himanshu04210 <himanshupoly76075@gmail.com>
2023-05-03 23:12:34 +0530

    Student crud operation

--- Student_crud_operation/student_crud_frontend_627827/student_crud_frontend/js/getAllStudents.js
+++ Student_crud_operation/student_crud_frontend_627827/student_crud_frontend/js/getAllStudents.js
@@ -1,62 +1,63 @@
-function f1(){
+function f1() {
 
     const studentTableBody = document.getElementById('student-table-body');
 
     fetch("http://localhost:8080/students").then(response => {
-        if(response.status == 201 | response.status == 200){
+        if (response.status == 201 | response.status == 200) {
             response.json().then(data => {
-               
-               data.forEach(student =>{
 
-                const row = document.createElement('tr');
-      row.innerHTML = `
-        <td>${student.roll}</td> 
-        <td>${student.name}</td>
-        <td>${student.address}</td>
-        <td>${student.marks}</td>
-        <td><button button style ="background-color:yellow" onclick="updateRecord('${student.name}','${student.address}','${student.marks}')">UPDATE</button> </td>
-        <td><button style ="background-color:red" onclick="deleteRecord(${student.roll})">DELETE</button> </td>
-      `;
-      studentTableBody.appendChild(row);
-    
-     })})
-        }else{
+                data.forEach(student => {
+
+                    const row = document.createElement('tr');
+                    row.innerHTML = `
+                    <td>${student.roll}</td> 
+                    <td>${student.name}</td>
+                    <td>${student.address}</td>
+                    <td>${student.marks}</td>
+                    <td><button button style ="background-color:yellow" onclick="updateRecord('${student.name}','${student.address}','${student.marks}')">UPDATE</button> </td>
+                    <td><button style ="background-color:red" onclick="deleteRecord(${student.roll})">DELETE</button> </td>
+                    `;
+                    studentTableBody.appendChild(row);
+
+                })
+            })
+        } else {
             response.json().then(data => alert(data.message));
         }
     })
 
 }
 
-function deleteRecord(roll){
-    
-    let choice= confirm("Are You Sure ?");
+function deleteRecord(roll) {
 
-    if(choice){
-    fetch("http://localhost:8080/students/"+roll, {
+    let choice = confirm("Are You Sure ?");
 
-    method: "DELETE"
-   
-}).then(response => {
-    if(response.status == 201 | response.status == 200){
-        response.json().then(data => {
-            alert("Student sucessfully deleted: ");
-            location.reload();
-        });
-    }else{
-        response.json().then(data => alert(data.message));
-    }
-})
+    if (choice) {
+        fetch("http://localhost:8080/students/" + roll, {
+
+            method: "DELETE"
+
+        }).then(response => {
+            if (response.status == 201 | response.status == 200) {
+                response.json().then(data => {
+                    alert("Student sucessfully deleted: ");
+                    location.reload();
+                });
+            } else {
+                response.json().then(data => alert(data.message));
+            }
+        })
     }
 
 }
 
 
-function updateRecord(name,address,marks) {
-   
+function updateRecord(name, address, marks) {
+
     const url = `updateStudent.html?name=${name}&address=${address}&marks=${marks}`;
 
-    location.href= url;
+    location.href = url;
 
-   
-  }
+
+}
 
