b288ac4bb4f34cd912f0d0f6154614a12bf250e8
Himanshu04210 <himanshupoly76075@gmail.com>
2023-05-08 18:30:59 +0530

    Spring boot app

---
+++ springbootjpaapp1_256560_LiveClass/springbootjpaapp1/src/main/java/com/masai/Springbootjpaapp1Application.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootjpaapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootjpaapp1Application.class, args);
+	}
+
+}
