2b9942ace53295638b7226f5fffefbb8f8357a26
Himanshu04210 <himanshupoly76075@gmail.com>
2023-06-05 15:31:35 +0530

    created cotroller and cofigure package

---
+++ SpringSecurity1/src/main/java/com/masai/Controller/MainController.java
@@ -0,0 +1,32 @@
+package com.masai.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class MainController {
+	
+	@GetMapping("/welcome")
+	public ResponseEntity<String> welcome(){
+		
+	 return new ResponseEntity<String>("Welcome to Masai App without security",HttpStatus.ACCEPTED);
+
+	}
+		
+	
+	@GetMapping("/welcomeP")
+	public ResponseEntity<String> welcomeP(){
+		
+			return new ResponseEntity<String>("Welcome to Masai App with Security",HttpStatus.ACCEPTED);
+	}
+
+	@GetMapping("/admin")
+	public ResponseEntity<String> admin(){
+		
+		return new ResponseEntity<String>("Welcome to Masai App for Admin",HttpStatus.ACCEPTED);
+		}
+	
+}
