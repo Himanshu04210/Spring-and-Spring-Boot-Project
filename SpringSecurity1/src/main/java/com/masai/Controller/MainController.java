196ff3f768521157bbf3822b0d90697ceb8f81a9
Himanshu04210 <himanshupoly76075@gmail.com>
2023-06-16 15:50:59 +0530

    working on security

--- SpringSecurity1/src/main/java/com/masai/Controller/MainController.java
+++ SpringSecurity1/src/main/java/com/masai/Controller/MainController.java
@@ -23,6 +23,9 @@ public class MainController {
 		
 			return new ResponseEntity<String>("Welcome to Masai App with Security",HttpStatus.ACCEPTED);
 	}
+	
+	
+	
 
 	@GetMapping("/admin")
 	public ResponseEntity<String> admin() {
