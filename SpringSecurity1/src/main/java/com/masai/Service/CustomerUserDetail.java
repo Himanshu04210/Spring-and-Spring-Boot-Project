b57ae1c123fa61771a542e373c576f3b208d8859
Himanshu04210 <himanshupoly76075@gmail.com>
2023-06-20 11:11:04 +0530

    some more work

--- SpringSecurity1/src/main/java/com/masai/Service/CustomerUserDetail.java
+++ SpringSecurity1/src/main/java/com/masai/Service/CustomerUserDetail.java
@@ -38,25 +38,21 @@ public class CustomerUserDetail implements UserDetails{
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
