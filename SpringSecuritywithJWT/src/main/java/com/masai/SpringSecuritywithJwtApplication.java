a58af78ec452a493eac90209ff4121aea22ea1a6
Himanshu04210 <himanshupoly76075@gmail.com>
2023-06-21 23:24:37 +0530

    working on new application which is based on spring security with jwt

---
+++ SpringSecuritywithJWT/src/main/java/com/masai/SpringSecuritywithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritywithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritywithJwtApplication.class, args);
+	}
+
+}
