ed8bd3b7eb7c37d5af19c0208fc5e3e53050f349
Himanshu04210 <himanshupoly76075@gmail.com>
2023-06-22 23:13:47 +0530

    working on configuration

--- SpringSecuritywithJWT/src/main/java/com/masai/Controller/LogginController.java
+++ SpringSecuritywithJWT/src/main/java/com/masai/Controller/LogginController.java
@@ -1,5 +1,33 @@
 package com.masai.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.masai.Model.Customer;
+import com.masai.Repository.CustomerRepository;
+
+@RestController
 public class LogginController {
+	
+	private CustomerRepository customerRepository;
+	
+	
+	@GetMapping("signIn")
+	public ResponseEntity<Customer> logInCustomerDetailHandler(Authentication authentication) {
+		
+		String username = authentication.getName();
+		
+		Customer customer = customerRepository.findByEmail(username).orElseThrow(() -> new BadCredentialsException("Email is not matched"));
+		
+		return new ResponseEntity<Customer>(customer, HttpStatus.FOUND);
+				
+		
+		
+	}
+	
 
 }
