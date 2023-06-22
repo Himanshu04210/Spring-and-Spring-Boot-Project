fb23512091998d51f1e00cf616fb582155a5afcf
Himanshu04210 <himanshupoly76075@gmail.com>
2023-06-22 23:14:04 +0530

    working on configuration

--- SpringSecuritywithJWT/src/main/java/com/masai/Configuration/AppConfig.java
+++ SpringSecuritywithJWT/src/main/java/com/masai/Configuration/AppConfig.java
@@ -1,13 +1,16 @@
 package com.masai.Configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class AppConfig {
 
 	@Bean
 	public SecurityFilterChain projectSecurity(HttpSecurity http) throws Exception {
+		
 		
 		
 		
