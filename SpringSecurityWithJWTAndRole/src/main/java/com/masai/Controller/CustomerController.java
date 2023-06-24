3906629145df2137686407eb933589ab88701cec
Himanshu04210 <himanshupoly76075@gmail.com>
2023-06-24 16:57:50 +0530

    normal structure is created

---
+++ SpringSecurityWithJWTAndRole/src/main/java/com/masai/Controller/CustomerController.java
@@ -0,0 +1,63 @@
+package com.masai.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.masai.Model.Customer;
+import com.masai.Service.CustomerService;
+
+@RestController
+public class CustomerController {
+	
+	@Autowired
+	private CustomerService customerService;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "welcome to the app";
+	}
+	
+	
+	@PostMapping("/customers")
+	public ResponseEntity<?> registerCustomer(@RequestBody Customer customer) {
+		
+		customer.setRole("ROLE_" + customer.getRole().toUpperCase());
+		customer.setPassword(passwordEncoder.encode(customer.getPassword()));
+		Customer savedCustomer = customerService.registerCustomer(customer);
+		
+		return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
+		
+	}
+	
+	
+	@GetMapping("/customers/{email}")
+	public ResponseEntity<Customer> getCustomerByEmailHandler(@PathVariable("email") String email) {
+		Customer customer = customerService.getCustomerByEmail(email);
+		
+		return new ResponseEntity<>(customer, HttpStatus.FOUND);
+		
+	}
+	
+	
+	@GetMapping("/customers")
+	public ResponseEntity<List<Customer>> getCustomerByEmail() {
+		List<Customer> customers = customerService.getAllCustomer();
+		
+		return new ResponseEntity<>(customers, HttpStatus.OK);
+		
+	}
+	
+	
+}
--- SpringSecurityWithJWTAndRole/src/main/java/com/masai/Model/Customer.java
+++ SpringSecurityWithJWTAndRole/src/main/java/com/masai/Model/Customer.java
@@ -1,5 +1,34 @@
 package com.masai.Model;
 
-public class Customer {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+
+
+@Entity
+@Data
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer customerId;
+	
+	private String name;
+	
+	@Email
+	@Column(unique = true)
+	private String email;
+	
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	private String password;
+	
+	private String address;
+	
+	private String role;
 }
